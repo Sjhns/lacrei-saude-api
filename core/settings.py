fa84cc176d9495fa982c9c9f2e55240aee858e6d
Johnson <108435026+Sjhns@users.noreply.github.com>
2025-10-25 11:47:25 -0300

    fix: update default ALLOWED_HOSTS to include elasticbeanstalk domain

--- core/settings.py
+++ core/settings.py
@@ -51,7 +51,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = (
-    os.getenv("ALLOWED_HOSTS").split(",") if os.getenv("ALLOWED_HOSTS") else []
+    os.getenv("ALLOWED_HOSTS").split(",") if os.getenv("ALLOWED_HOSTS") else ['.elasticbeanstalk.com', '*']
 )
 
 
